4b8f8bb4e8b4c7c45e7f5715b03c2dc502e774cb
pkc918 <xiaopan2000918@gmail.com>
2025-10-11 11:13:47 +0800

    feat: update Nuxt.js configuration with Tailwind CSS and new modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  modules: [
+    '@nuxt/ui',
+    '@nuxtjs/mdc',
+  ],
   devtools: { enabled: true },
   routeRules: {
     '/': { prerender: true }
-  }
+  },
+  css: ['~/assets/css/main.css'],
+  ui: {
+    theme: {
+      defaultVariants: {
+        color: 'neutral'
+      }
+    }
+  },
+  mdc: {
+    highlight: {
+      langs: ['diff', 'ts', 'vue', 'css']
+    },
+    remarkPlugins: {
+      'remark-github': {
+        options: {
+          repository: 'nuxt-ui-templates/changelog'
+        }
+      }
+    }
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
