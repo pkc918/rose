f14adff520a46ffa0ab55e147242142edbdc0eb6
pkc918 <xiaopan2000918@gmail.com>
2025-10-14 08:26:32 +0800

    refactor: simplify Header component by removing dynamic links and updating layout in index.vue

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,32 +1,4 @@
 <script setup lang="ts">
-interface Link {
-    href: string
-    title: string
-    Icon: string
-}
-const links: Link[] = [
-    {
-        href: '/blog',
-        title: 'Blog',
-        Icon: ''
-    },
-    {
-        href: '/diary',
-        title: 'Diary',
-        Icon: ''
-    },
-    {
-        href: '/photos',
-        title: '',
-        Icon: 'i-tabler-brand-google-photos'
-    },
-    {
-        href: 'https://github.com/pkc918',
-        title: '',
-        Icon: 'i-tabler-brand-github'
-    },
-]
-
 function toggleTheme() {
     // useNuxtApp().$toggleTheme()
 }
@@ -35,9 +7,17 @@ function toggleTheme() {
 <template>
 <header w-full flex="~ justify-end items-center" class="box-border p-[2rem]">
     <nav grid="~ gap-[1.2rem] auto-flow-col">
-        <a v-for="link in links" :key="link.href" :href="link.href">
-            <div w-6 h-6 v-if="link.Icon" :class="link.Icon"></div>
-            <div v-else>{{ link.title }}</div>
+        <a href="/blog" title="Blog">
+            <div font-bold>Blog</div>
+        </a>
+        <a href="/diary" title="Diary">
+            <div font-bold>Diary</div>
+        </a>
+        <a href="/photos" title="Photos">
+            <div w-6 h-6 i-tabler-brand-google-photos></div>
+        </a>
+        <a href="https://github.com/pkc918" target="_blank" title="Github">
+            <div w-6 h-6 i-tabler-brand-github></div>
         </a>
         <a title="Toggle Theme" @click="toggleTheme">
             <div w-6 h-6 i-tabler-brightness-up dark:i-tabler-brightness-up-filled></div>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -21,33 +21,14 @@ const { data: versions } = await useFetch(computed(() => `https://ungh.cc/repos/
 </script>
 
 <template>
-  <UChangelogVersions
-    as="main"
-    :indicator-motion="false"
-    :ui="{
-      root: 'py-16 sm:py-24 lg:py-32',
-      indicator: 'inset-y-0'
-    }"
-  >
-    <UChangelogVersion
-      v-for="version in versions"
-      :key="version.tag"
-      v-bind="version"
-      :ui="{
-        root: 'flex items-start',
-        container: 'max-w-xl',
-        header: 'border-b border-default pb-4',
-        title: 'text-3xl',
-        date: 'text-xs/9 text-highlighted font-mono',
-        indicator: 'sticky top-0 pt-16 -mt-16 sm:pt-24 sm:-mt-24 lg:pt-32 lg:-mt-32'
-      }"
-    >
-      <template #body>
-        <MDC
-          v-if="version.markdown"
-          :value="version.markdown"
-        />
-      </template>
-    </UChangelogVersion>
-  </UChangelogVersions>
+<div w-80vw max-w-80vw min-w-300 min-h-screen flex="~ items-center justify-center">
+    <div w-full h-full flex="~ justify-between">
+        <div class="w-1/2">
+            <h1>Hello World</h1>
+        </div>
+        <div class="w-1/2">
+            <h1>Hello World</h1>
+        </div>
+    </div>
+</div>
 </template>
