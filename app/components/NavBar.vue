34c92225edab1ed81dc008b57bd52b1a21d6a57e
pkc918 <xiaopan2000918@gmail.com>
2025-10-14 20:41:41 +0800

    feat: enhance NavBar component with avatar image

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 function toggleTheme() {
     // useNuxtApp().$toggleTheme()
-}
+}   
 </script>
 
 <template>
-    <nav flex="~ col justify-center items-center gap-[1.2rem]" class="[&_a]:w-8 [&_a]:h-8 [&_a]:fcc">
+    <div flex="~ col justify-center items-center gap-[1.2rem]">
+        <div w-12 h-12 p-1 rounded-full border="~ 2px solid black-200" class="fcc">
+            <img src="/avatar.jpeg" alt="logo" class="w-10 h-10 rounded-full" />
+        </div>
+        <nav flex="~ col justify-center items-center gap-[1.2rem]" class="[&_a]:w-8 [&_a]:h-8 [&_a]:fcc">
         <a href="/blog" title="Blog">
             <div w-6 h-6 i-tabler-brand-blogger />
         </a>
@@ -22,4 +26,5 @@ function toggleTheme() {
             <div w-6 h-6 i-tabler-brightness-up dark:i-tabler-brightness-up-filled />
         </a>
     </nav>
+    </div>
 </template>
