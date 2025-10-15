9cd887dff90effa32a64e8ad9ff904e84e9ab577
pkc918 <xiaopan2000918@gmail.com>
2025-10-15 21:26:47 +0800

    feat: add button color shortcut to uno.config for improved styling options

--- uno.config.ts
+++ uno.config.ts
@@ -12,6 +12,7 @@ import {
 export default defineConfig({
   shortcuts: [
     ['fcc', 'flex justify-center items-center'],
+    [/^btn-(\w+)$/, ([_, color]) => `op50 px2.5 py1 transition-all duration-200 ease-out no-underline! hover:(op100 text-${color} bg-${color}/10) border border-base! rounded`],
   ],
   presets: [
     presetUno(),
