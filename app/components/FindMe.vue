f4663fabd9a6e31acd2c1db1eea345b4412db32a
pkc918 <xiaopan2000918@gmail.com>
2025-10-17 12:44:21 +0800

    feat: enhance FindMe component with additional social links and improved layout

--- app/components/FindMe.vue
+++ app/components/FindMe.vue
@@ -5,11 +5,31 @@
 <template>
     <div flex="~ col gap-5">
         <p>Find me on</p>
-        <div flex="~ items-center gap-[1.2rem]">
-            <div flex="~ items-center justify-center">
+        <div flex="~ items-center gap-[2rem]">
+            <a href="https://github.com/pkc918" target="_blank" flex="~ items-center justify-center gap-1">
                 <div w-6 h-6 i-tabler-brand-github />
                 <div>Github</div>
-            </div>
+            </a>
+
+            <a href="https://github.com/pkc918" target="_blank" flex="~ items-center justify-center gap-1">
+                <div w-6 h-6 i-tabler-brand-discord />
+                <div>Discord</div>
+            </a>
+
+            <a href="https://x.com/pkc918" target="_blank" flex="~ items-center justify-center gap-1">
+                <div w-6 h-6 i-tabler-brand-x />
+                <div>青椒肉丝</div>
+            </a>
+
+            <a target="_blank" flex="~ items-center justify-center gap-1">
+                <div w-6 h-6 i-tabler-brand-wechat />
+                <div>pkc0918</div>
+            </a>
+
+            <a href="mailto:xiaopan2000918@gmail.com" target="_blank" flex="~ items-center justify-center gap-1">
+                <div w-6 h-6 i-tabler-mail-opened />
+                <div>xiaopan2000918@gmail.com</div>
+            </a>
         </div>
     </div>
 </template>
