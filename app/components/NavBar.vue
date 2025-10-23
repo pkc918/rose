63300e2e922714796e5f0e1a4eb8774569777fb2
pkc918 <xiaopan2000918@gmail.com>
2025-10-23 17:14:42 +0800

    style(NavBar): enhance hover effects

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -10,25 +10,25 @@ function backHome() {
 
 <template>
     <div flex="~ col justify-center items-center gap-[1.2rem]">
-        <div rounded-full border="~ 2px solid black-200" class="fcc cursor-pointer" @click="backHome">
+        <div hover:scale-125 duration-300 rounded-full border="~ 2px solid black-200" class="fcc cursor-pointer" @click="backHome">
             <img src="/avatar.jpeg" alt="logo" class="w-10 h-10 rounded-full" />
         </div>
         <nav flex="~ col justify-center items-center gap-[1.2rem]" class="[&_a]:w-8 [&_a]:h-8 [&_a]:fcc">
-        <a href="/blogs" title="Blog">
-            <div w-6 h-6 i-tabler-brand-blogger />
-        </a>
-        <a href="/diaries" title="Diary">
-            <div w-6 h-6 i-tabler-lifebuoy />
-        </a>
-        <a href="/photos" title="Photos">
-            <div w-6 h-6 i-tabler-brand-google-photos />
-        </a>
-        <a href="https://github.com/pkc918" target="_blank" title="Github">
-            <div w-6 h-6 i-tabler-brand-github />
-        </a>
-        <a cursor-pointer title="Toggle Theme" @click="toggleTheme">
-            <div w-6 h-6 i-tabler-brightness-up dark:i-tabler-brightness-up-filled />
-        </a>
-    </nav>
+            <a hover:scale-140 duration-300  href="/blogs" title="Blog">
+                <div w-6 h-6 i-tabler-brand-blogger />
+            </a>
+            <a hover:scale-140 duration-300  href="/diaries" title="Diary">
+                <div w-6 h-6 i-tabler-lifebuoy />
+            </a>
+            <a hover:scale-140 duration-300  href="/photos" title="Photos">
+                <div w-6 h-6 i-tabler-brand-google-photos />
+            </a>
+            <a hover:scale-140 duration-300 href="https://github.com/pkc918" target="_blank" title="Github">
+                <div w-6 h-6 i-tabler-brand-github />
+            </a>
+            <a hover:scale-140 duration-300 cursor-pointer title="Toggle Theme" @click="toggleTheme">
+                <div w-6 h-6 i-tabler-brightness-up dark:i-tabler-brightness-up-filled />
+            </a>
+        </nav>
     </div>
 </template>
