50a05289ec283d8b7965750db4538f8003a8bec8
pkc918 <xiaopan2000918@gmail.com>
2025-10-24 19:45:42 +0800

    fix(blog): add optional chaining for blog properties and improve date formatting

--- app/pages/blogs/[title].vue
+++ app/pages/blogs/[title].vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { formatDate } from '~/utils/formatTime'
+
 const route = useRoute()
 
 // 获取具体的博客内容
@@ -13,10 +15,10 @@ const { data: blog } = await useAsyncData(route.path, () => {
 <div min-h-screen min-w-0 flex="~ col" py-10>
   <section class="prose prose-truegray dark:prose-invert max-w-full! text-justify pt-20" >
     <div class="mb-20">
-      <h1 class="mb-0! text-black dark:text-white">{{ blog.title }}</h1>
+      <h1 class="mb-0! text-black dark:text-white">{{ blog?.title }}</h1>
       <div class="mt-5 text-gray-600 dark:text-gray-500" flex="~ items-center gap-4" font-mono text-xs opacity-50>
-        <span>{{ formatDate(blog.date) }}</span>
-        <span>{{ blog.duration }}</span>
+        <span>{{ blog?.date ? formatDate(blog.date) : '' }}</span>
+        <span>{{ blog?.duration }}</span>
       </div>
     </div>
 
